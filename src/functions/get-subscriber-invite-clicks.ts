3216b921fff895c69a0877fbbde353350f086ee1
EsleySaab <esleysantana.sa@gmail.com>
2025-04-09 19:04:30 -0300

    feat: create get subscriber invite clicks function and route

---
+++ src/functions/get-subscriber-invite-clicks.ts
@@ -0,0 +1,17 @@
+import { db } from "../drizzle/client"
+import { subscriptions } from "../drizzle/schema/subscriptions"
+import { redis } from "../redis/client"
+
+interface GetSubscriberInviteClicksParams {
+  subscriberId: string
+}
+
+export async function getSubscriberInviteCLicks({
+  subscriberId,
+}: GetSubscriberInviteClicksParams) {
+  const count = await redis.hget("referral:access-count", subscriberId)
+
+  return {
+    count: count ? Number.parseInt(count) : 0,
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -10,8 +10,9 @@ import {
   validatorCompiler,
 } from "fastify-type-provider-zod"
 import { env } from "./env"
-import { accessInviteLinkRoute } from "./routes/access-invite-link"
+import { accessInviteLinkRoute } from "./routes/access-invite-link-route"
 import { subscribeToEventRoute } from "./routes/subscribe-to-event-route"
+import { getSubscriberInviteClicksRoute } from "./routes/get-subscriber-invite-clicks-route"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -36,6 +37,7 @@ app.register(fastifySwaggerUi, {
 
 app.register(subscribeToEventRoute)
 app.register(accessInviteLinkRoute)
+app.register(getSubscriberInviteClicksRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log("HTTP Server Running!")
